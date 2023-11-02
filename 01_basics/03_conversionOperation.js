de0de5fa6c706b07b367a32058bfe183477ff0a1
Preeti <73287636+preetipie22@users.noreply.github.com>
2023-11-02 07:41:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = NaN;
+
+// console.log(typeof score);
+// console.log(Number(score));
+
+//1 => true, 0 => false
+//"" => 0
+//"preeti" => NaN
+//"33abc" =>NaN
+
+// console.log(typeof score);
+// console.log(Number(score));
+
+//1 => true, 0 => false
+//"" => false
+//"preeti" => true
+
+console.log(typeof score);
+console.log(String(score));
+
+//33 => 33
